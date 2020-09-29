e7e2bb8456fddb69458aa8c9623d319f033af884
antcha19 <yepez261@gmail.com>
2020-09-29 18:40:55 +0200

    ejer

--- Suma/app/src/main/java/com/example/suma/MainActivity2.java
+++ Suma/app/src/main/java/com/example/suma/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.suma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -14,7 +17,10 @@ public class MainActivity2 extends AppCompatActivity {
 
         final TextView resultadofinal =(TextView)findViewById(R.id.resultadofinal);
 
+
         Bundle miBundlefinal = getIntent().getExtras();
         resultadofinal.setText(miBundlefinal.getString("TEXTO"));
+
+
     }
 }
